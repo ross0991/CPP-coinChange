148aeeeeed7ad676532f974ebf3679516da75dd5
David <56272724+ross0991@users.noreply.github.com>
2020-12-24 01:27:59 +0000

    Update coinChange.cpp

--- coinChange.cpp
+++ coinChange.cpp
@@ -16,3 +16,23 @@ void initialise(){
     ready[i] = false;
   }
 };
+
+int solve(int x){
+  if(x < 0) return MAXN;
+  if(x == 0) return 0;
+  if(ready[x]) return value[x];
+  
+  int best = MAXN;
+  for(auto c :coins){
+    best = min(best, solve(x-c) +1);
+  }
+  ready[x] = true;
+  value[x] = best;
+  return best;
+};
+
+int main(){
+  initialise();
+  cout << solve(10);
+  return 0;
+}
